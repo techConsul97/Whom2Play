c59e61976dd264420881c8b9000fd2d1cc824eac
techConsul97 <ittechconsultantemail@gmail.com>
2022-10-25 10:12:24 +0100

    Initial Commit
    Created Package Structure for initial modules

---
+++ app/src/main/java/com/sebqv97/whom2play/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.sebqv97.whom2play
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sebqv97.whom2play.common.compose_themes.ui.theme.Whom2PlayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Whom2PlayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Whom2PlayTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/sebqv97/whom2play/common/compose_themes/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.sebqv97.whom2play.common.compose_themes.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
